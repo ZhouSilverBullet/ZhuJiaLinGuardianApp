3113c9e4b26d02e3925d9cdb40291e6573990947
zhousaito <zhousaito@163.com>
2019-05-15 23:58:10 +0800

    toolbar为title，然后进行AppBar加入阴影效果
    toolbar如果是setSupportActionBar,toolbar的inflateMenu就会失效，要重写activity的onCreateOptionsMenu方法来进行inflateMenu
    AppBar设置阴影效果

--- app/src/main/java/com/sdxxtop/zjlguardian/ui/home/HomeActivity.kt
+++ app/src/main/java/com/sdxxtop/zjlguardian/ui/home/HomeActivity.kt
@@ -1,21 +1,48 @@
 package com.sdxxtop.zjlguardian.ui.home
 
 
+import android.view.Menu
 import com.sdxxtop.zjlguardian.R
 import com.sdxxtop.zjlguardian.base.KBaseActivity
 import com.sdxxtop.zjlguardian.databinding.ActivityHomeBinding
+import com.sdxxtop.zjlguardian.extens.toast
+import android.view.MenuItem
+
 
 class HomeActivity : KBaseActivity<ActivityHomeBinding>() {
 
-    override fun getLayoutId() = R.layout.activity_home
+    override fun getLayoutId() = com.sdxxtop.zjlguardian.R.layout.activity_home
 
     override fun loadData(isRefresh: Boolean) {
 
     }
 
     override fun initView() {
-        mBinding.toolbar.title = ""
-        setSupportActionBar(mBinding.toolbar)
+        var toolbar = mBinding.toolbar
+        toolbar.title = ""
+
+        setSupportActionBar(toolbar)
+
+        toolbar.setOnClickListener { toast("asfasfd") }
+
+        toolbar.setNavigationOnClickListener { view ->
+            toast("fanhui")
+        }
+
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            toast("afdaf")
+            return@setOnMenuItemClickListener false
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.right_icon, menu)
+        return true
     }
 
 
