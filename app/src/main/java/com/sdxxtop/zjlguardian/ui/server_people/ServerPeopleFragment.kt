c17b5e1bf21d6d459ea9b749d5dc4b42bccacf14
zhousaito <zhousaito@163.com>
2019-05-24 15:16:48 +0800

    首页界面修改

--- app/src/main/java/com/sdxxtop/zjlguardian/ui/server_people/ServerPeopleFragment.kt
+++ app/src/main/java/com/sdxxtop/zjlguardian/ui/server_people/ServerPeopleFragment.kt
@@ -1,21 +1,30 @@
 package com.sdxxtop.zjlguardian.ui.server_people
 
 import android.content.Context
+import android.graphics.Color
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.sdxxtop.zjlguardian.R
+import com.google.android.material.tabs.TabLayout
 import com.sdxxtop.zjlguardian.base.KBaseFragment
 import com.sdxxtop.zjlguardian.databinding.FragmentServerPeopleBinding
+import com.sdxxtop.zjlguardian.helper.adapter.HomePagerAdapter
 import com.sdxxtop.zjlguardian.ui.politics.PoliticsActivity
 import com.sdxxtop.zjlguardian.widget.AutoTextViewManager
 import com.youth.banner.loader.ImageLoader
 import kotlinx.android.synthetic.main.fragment_server_people.*
 import org.jetbrains.anko.support.v4.startActivity
+import android.widget.LinearLayout
+import android.util.TypedValue
+import android.view.LayoutInflater
+import com.sdxxtop.zjlguardian.R
 
 /**
  * A simple [Fragment] subclass.
@@ -39,6 +48,47 @@ class ServerPeopleFragment : KBaseFragment<FragmentServerPeopleBinding>() {
 
             autoTextViewManager.setData(it.notices)
             autoTextViewManager.start()
+        })
+
+        val content = "最新公告";
+        var stringBuilder = SpannableStringBuilder(content);
+        val foregroundColorSpan = ForegroundColorSpan(Color.parseColor("#FF4040"));
+        stringBuilder.setSpan(foregroundColorSpan, 1, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mBinding.tvTitle1.text = stringBuilder;
+
+        initTab()
+    }
+
+    private fun initTab() {
+
+        val arrayList = listOf("户政", "社保", "补助", "婚姻", "计生", "生活")
+        val fragmentList = listOf(ServerTabFragment(), ServerTabFragment(),
+                ServerTabFragment(), ServerTabFragment(), ServerTabFragment(), ServerTabFragment())
+
+
+        val homePagerAdapter = HomePagerAdapter(fragmentManager, arrayList, fragmentList)
+        mBinding.vp.adapter = homePagerAdapter
+        mBinding.tlTab.setupWithViewPager(mBinding.vp)
+
+        val title = ((mBinding.tlTab.getChildAt(0) as LinearLayout).getChildAt(0) as LinearLayout).getChildAt(1) as TextView
+        title.textSize = 18f
+        title.setTextAppearance(activity, R.style.AppliedTabLayoutTextStyle)
+        mBinding.tlTab.addOnTabSelectedListener(object : TabLayout.BaseOnTabSelectedListener<TabLayout.Tab> {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                tab?.customView = null
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val textView = LayoutInflater.from(mContext).inflate(com.sdxxtop.zjlguardian.R.layout.tab_text_layout, null) as TextView
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
+                textView.text = tab?.text
+                tab?.customView = textView
+            }
+
         })
 
     }
