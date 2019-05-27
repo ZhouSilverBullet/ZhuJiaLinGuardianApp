e9320ee1d52d88f1a9151edd1c1f852a0d6e6f70
zhousaito <zhousaito@163.com>
2019-05-27 14:09:56 +0800

    我的界面修改2

--- app/src/main/java/com/sdxxtop/zjlguardian/ui/politics/PoliticsDetailActivity.kt
+++ app/src/main/java/com/sdxxtop/zjlguardian/ui/politics/PoliticsDetailActivity.kt
@@ -2,6 +2,7 @@ package com.sdxxtop.zjlguardian.ui.politics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sdxxtop.zjlguardian.R
 
 class PoliticsDetailActivity : AppCompatActivity() {
 
