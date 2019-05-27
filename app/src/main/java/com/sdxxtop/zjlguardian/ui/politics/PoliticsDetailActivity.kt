eeea1830eaf6ccd1c7cac2d0c12db6004a854f51
zhousaito <zhousaito@163.com>
2019-05-27 14:09:37 +0800

    我的界面修改

---
+++ app/src/main/java/com/sdxxtop/zjlguardian/ui/politics/PoliticsDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.sdxxtop.zjlguardian.ui.politics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PoliticsDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_politics_detail)
+    }
+}
